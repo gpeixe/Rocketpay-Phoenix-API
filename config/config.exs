521e872d6a8a3715101215673c4b2177eef1500b
peixe <guilherme.o.peixe@gmail.com>
2021-02-25 02:33:47 -0300

    add bcrypt

--- config/config.exs
+++ config/config.exs
@@ -18,7 +18,11 @@ config :rocketpay, RocketpayWeb.Endpoint,
   pubsub_server: Rocketpay.PubSub,
   live_view: [signing_salt: "wXym9pe1"]
 
+config :rocketpay, Rocketpay.Repo,
+  migration_primary_key: [type: :binary_id],
+  migration_foreign_key: [type: :binary_id]
 # Configures Elixir's Logger
+
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
--- mix.exs
+++ mix.exs
@@ -43,10 +43,10 @@ defmodule Rocketpay.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:bcrypt_elixir, "~> 2.0"}
     ]
   end
-
   # Aliases are shortcuts or tasks specific to the current project.
   # For example, to install project dependencies and perform other setup tasks, run:
   #
